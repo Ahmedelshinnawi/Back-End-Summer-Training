2a88dbe2addf5b28c80e2004602e48f4ef239337
Ahmed Elshinnawi <101606752+Ahmedelshinnawi@users.noreply.github.com>
2024-08-27 14:50:41 +0300

    Delete JavaScript/Random Post/Third Task.js

--- JavaScript/Random Post/Third Task.js
+++
@@ -1,8 +0,0 @@
-let Data = async () => {
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let data = await response.json();
-    let random = data[Math.floor(Math.random() * data.length)];
-    document.getElementById('Post').innerText = random.body;
-}
-Data();
-setInterval(Data, 60000);
